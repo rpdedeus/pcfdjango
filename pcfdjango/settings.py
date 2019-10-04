7f62a6c1dafc843c631063288f4094f553943992
rpdedeus <roberto.p.dedeus@gmail.com>
2019-10-04 18:35:27 -0400

    Add dependency whitenoise to allow the application to serve its own static files.

--- pcfdjango/settings.py
+++ pcfdjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/staticfiles/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
